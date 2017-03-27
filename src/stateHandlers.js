b718474c0b0c2f057a165d2d8a9937bbb6ace243
Simon Shirley <email@simonshirley.uk>
2017-03-27 16:52:09 +0100

    Game start mode and game setup

--- src/stateHandlers.js
+++ src/stateHandlers.js
@@ -2,14 +2,59 @@
 
 var Alexa = require("alexa-sdk");
 var constants = require("./constants");
+var strings = require("./strings");
+
+var colors = ["red", "green", "yellow", "blue"];
 
 var stateHandlers = {
     startModeStateHandlers: Alexa.CreateStateHandler(constants.states.START_MODE, {
-
+        'LaunchRequest': function() {
+            this.handler.state = constants.states.START_MODE;
+            this.response.listen(strings.launch_message);
+            this.emit(":responseReady");
+        },
+        'ColorMatchGame': function() {
+            this.handler.state = constants.states.GUESS_MODE;
+            this.emitWithState('ColorMatchGameStart');
+        },
+        'AMAZON.YesIntent': function() {
+            this.handler.state = constants.states.GUESS_MODE;
+            this.emitWithState('ColorMatchGameStart');
+        },
+        'AMAZON.NoIntent': function() {
+            this.handler.state = constants.states.START_MODE;
+            this.response.speak(strings.exit_message);
+            this.emit(":responseReady");
+        }
     }),
     guessModeStateHandlers: Alexa.CreateStateHandler(constants.states.GUESS_MODE, {
+        'LaunchRequest': function() {
+            this.handler.state = constants.states.START_MODE;
+            this.emitWithState('LaunchRequest');
+        },
+        'ColorMatchGameStart': function() {
+            // initialize attributes
+            helpers.reset.call(this);
 
+            // generate a random number between 0 and 3
+            var randomColorIndex = Math.floor(Math.random() * 3);
+            this.attributes["computedPattern"] = this.attributes["computedPattern"].push(colors[randomColorIndex]);
+
+            this.response.listen(this.attributes["computedPattern"].join(','));
+            this.emit(":responseReady");
+        }
     })
 };
 
 module.exports = stateHandlers;
+
+var helpers = function() {
+    return {
+        reset: function() {
+            this.attributes["computedPattern"] = [];
+            this.attributes["guessPattern"] = [];
+            this.attributes["currentScore"] = 0;
+            this.attributes["winScore"] = 10;
+        }
+    }
+}
---
+++ src/strings.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var strings = {
+    launch_message: "Welcome to the Color Match Game. See how long you can keep the chain going! Would you like to play?",
+    game_start_message: "I will announce a series of colors. All you have to do is to repeat them back to me, one at a time. Let's begin...",
+    exit_message: "Goodbye."
+};
+
+module.exports = strings;
